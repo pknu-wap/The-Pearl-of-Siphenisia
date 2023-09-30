d7e47a78e56550e9783f33320b3bc3dccae4771c
Vedellan <61517039+Vedellan@users.noreply.github.com>
2023-09-30 17:12:50 +0900

    로비 씬 생성
    - 로비 씬 생성
    - 배경, 타이틀 이미지, 시작 버튼 생성
    - SceneHandler 생성(SceneManager는 UnityEngine.SceneManagement에 존재)
    - ItemTest, Lobby 씬을 빌드 세팅에서 등록하고 간단한 이동을 구현

---
+++ Assets/03. Scripts/Managers/SceneHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (transform.parent != null)
+        {
+            DontDestroyOnLoad(transform.root.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
