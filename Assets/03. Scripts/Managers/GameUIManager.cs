7a6cdb2bd1bb3ebe84c132aee998c3470661918c
Vedellan <61517039+Vedellan@users.noreply.github.com>
2023-10-04 15:09:36 +0900

    Manager 분리
    - SceneHandler를 SceneLoader로 변경
    - UIManager를 GameUIManager랑 LobbyUIManager로 변경
    - SampleScene 삭제

--- Assets/03. Scripts/Managers/GameUIManager.cs
+++ Assets/03. Scripts/Managers/GameUIManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : Singleton<UIManager>
+public class GameUIManager : Singleton<GameUIManager>
 {
     GameObject inventoryObj;
 
--- Assets/03. Scripts/Managers/SceneLoader.cs
+++ Assets/03. Scripts/Managers/SceneLoader.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneHandler : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
     private void Awake()
     {
