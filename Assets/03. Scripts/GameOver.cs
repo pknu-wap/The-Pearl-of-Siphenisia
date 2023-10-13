b69c82fa8a1f3a251125724851da98942b542eab
LightningXQ <jihoj20030501@gmail.com>
2023-10-13 12:36:01 +0900

    asdf
    asdf

---
+++ Assets/03. Scripts/GameOver.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public void PlayerInit()
+    {
+        Debug.Log("게임오버 실행");
+    }
+}
---
+++ Assets/03. Scripts/ObjTest.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Obj", menuName = "ObjTest")]
+public class ObjTest : ScriptableObject
+{
+    public int health;
+    public int armor;
+}
