a4911cb80fb2c4086116cfa7caeaa133358c2dab
Vedellan <61517039+Vedellan@users.noreply.github.com>
2023-10-27 16:27:07 +0900

    MoveTest.cs 수정
    - MoveTest의 WASD가 뒤섞여 있던 문제를 해결

--- Assets/03. Scripts/MoveTest.cs
+++ Assets/03. Scripts/MoveTest.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -28,25 +24,25 @@ public class MoveTest : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.A))
         {
             //transform.position += new Vector3(-1 * speed * Time.deltaTime, 0, 0);
             rig2d.AddForce(new Vector2(-1 * speed, 0), ForceMode2D.Force);
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.D))
         {
             //transform.position += new Vector3(1 * speed * Time.deltaTime, 0, 0);
             rig2d.AddForce(new Vector2(1 * speed, 0), ForceMode2D.Force);
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.W))
         {
             //transform.position += new Vector3(0, 1 * speed * Time.deltaTime, 0);
             rig2d.AddForce(new Vector2(0, 1 * speed), ForceMode2D.Force);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.S))
         {
             //transform.position += new Vector3(0, -1 * speed * Time.deltaTime, 0);
             rig2d.AddForce(new Vector2(0, -1 * speed), ForceMode2D.Force);
