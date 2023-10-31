fa0868af4820a87b2db305442481055755cebb65
YIM2UL2ET <132066506+YIM2UL2ET@users.noreply.github.com>
2023-10-31 15:16:17 +0900

    change pushableTile logic
    밀 수 있는 블럭 로직 변경

--- Assets/03. Scripts/pushableTile.cs
+++ Assets/03. Scripts/pushableTile.cs
@@ -5,28 +5,54 @@ using UnityEngine;
 
 public class pushableTile : MonoBehaviour
 {
-    Rigidbody2D rb2d;
-    private void Start()
+    Transform player; // 플레이어의 Transform 컴포넌트
+    Transform myTransform; // 오브젝트의 Transform 컴포넌트
+
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Vector2 normal = collision.contacts[0].normal;  
+
+            if (Mathf.Abs(normal.x) > Mathf.Abs(normal.y))
+            {
+                if (normal.x > 0)
+                {
+                    Debug.Log("플레이어가 왼쪽에 있습니다.");
+                    MoveObject(Vector2.right);
+                }
+                else
+                {
+                    Debug.Log("플레이어가 오른쪽에 있습니다.");
+                    MoveObject(Vector2.left);
+                }
+            }
+            else
+            {
+                if (normal.y > 0)
+                {
+                    Debug.Log("플레이어가 아래에 있습니다.");
+                    MoveObject(Vector2.up); 
+                }
+                else
+                {
+                    Debug.Log("플레이어가 위에 있습니다.");
+                    MoveObject(Vector2.down);
+                }
+            }
+        }
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    private void MoveObject(Vector2 direction)
     {
-        if (other.rigidbody.mass >= 0.15f)
-        {
-            Debug.Log("질량이 10 이상이므로 밀기 가능");
-            rb2d.bodyType = RigidbodyType2D.Dynamic;
-        }
-        else
-        {
-            Debug.Log("질량이 10 이하이므로 밀기 불가");
-        }
+        // 오브젝트의 위치를 원하는 방향으로 이동합니다.
+        float moveDistance = 0.05f; // 이동 거리 설정
+        transform.Translate(direction * moveDistance);
     }
 
     private void OnCollisionExit2D(Collision2D other)
     {
         Debug.Log("플레이어가 pushable tile 에서 멀어짐");
-        rb2d.bodyType = RigidbodyType2D.Static;
     }
 }
