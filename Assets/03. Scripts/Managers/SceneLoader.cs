3dad504ce12cf79bc300f161b7305c7ae452bcf3
Vedellan <61517039+Vedellan@users.noreply.github.com>
2023-11-01 01:01:45 +0900

    SceneLoader 통합
    - 로드 씬 기능을 SceneLoader에 통합

--- Assets/03. Scripts/Managers/SceneLoader.cs
+++ Assets/03. Scripts/Managers/SceneLoader.cs
@@ -1,12 +1,18 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : Singleton<SceneLoader>
 {
     [SerializeField]
     private string loadSceneName;
     float fadeTime = 2f;
+    private GameObject fadeObject;
+    
+    [SerializeField]
+    private Image fadeBG;
 
     private void Awake()
     {
@@ -18,6 +24,15 @@ public class SceneLoader : Singleton<SceneLoader>
         {
             DontDestroyOnLoad(gameObject);
         }
+
+        AssignObjects();
+        fadeObject.SetActive(false);
+    }
+
+    void AssignObjects()
+    {
+        fadeObject = transform.GetChild(0).gameObject;
+        fadeBG = fadeObject.GetComponent<Image>();
     }
 
     public void LoadScene(string sceneName)
@@ -29,7 +44,7 @@ public class SceneLoader : Singleton<SceneLoader>
 
     private IEnumerator Load(string sceneName)
     {
-        yield return StartCoroutine(LobbyUIManager.Instance.FadeOut(fadeTime));
+        yield return StartCoroutine(FadeOut(fadeTime));
 
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
         operation.allowSceneActivation = false;
@@ -53,8 +68,42 @@ public class SceneLoader : Singleton<SceneLoader>
     {
         if (scene.name == loadSceneName)
         {
-            StartCoroutine(LobbyUIManager.Instance.FadeIn(fadeTime));
+            StartCoroutine(FadeIn(fadeTime));
             SceneManager.sceneLoaded -= LoadSceneEnd;
+        }
+    }
+
+    public IEnumerator FadeIn(float seconds)
+    {
+        fadeObject.SetActive(true);
+
+        float timer = 0f;
+        Color color = new Color(0, 0, 0, 1);
+
+        while (timer <= seconds)
+        {
+            yield return null;
+            timer += Time.unscaledDeltaTime;
+            color.a = Mathf.Lerp(1, 0, timer);
+            fadeBG.color = color;
+        }
+
+        fadeObject.SetActive(false);
+    }
+
+    public IEnumerator FadeOut(float seconds)
+    {
+        fadeObject.SetActive(true);
+
+        float timer = 0f;
+        Color color = new Color(0, 0, 0, 0);
+
+        while (timer <= seconds)
+        {
+            yield return null;
+            timer += Time.unscaledDeltaTime;
+            color.a = Mathf.Lerp(0, 1, timer);
+            fadeBG.color = color;
         }
     }
 }
