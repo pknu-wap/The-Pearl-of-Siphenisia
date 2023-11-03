505c317c05d2bfba328e2abf16804ecd7ee8ca03
Vedellan <61517039+Vedellan@users.noreply.github.com>
2023-11-03 16:13:41 +0900

    상어 이미지 추가
    - 상어 이미지 및 애니메이션 추가
    - 상어 콜라이더 조정
    - 상어가 플레이어를 바라보게 설정
    - 상어가 플레이어를 따라오게 개선

--- Assets/03. Scripts/Enemy.cs
+++ Assets/03. Scripts/Enemy.cs
@@ -1,34 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Enemy : MonoBehaviour
 {
-    Transform enemyTransform;
     Transform playerTransform;
-    public float enemySpeed = 0.02f;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        enemyTransform = GetComponent<Transform>();
-        playerTransform = GameObject.FindWithTag("Player").transform;
-    }
+    public float enemySpeed = 2f;
+    public float rotateSpeed = 10f;
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    void Awake()
     {
-        
+        playerTransform = GameObject.FindWithTag("Player").transform;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        followPlayer();
-        Debug.Log("Stay");
+        FollowPlayer();
+        Move();
     }
 
-    public void followPlayer()
+    public void FollowPlayer()
     {
-        transform.position = Vector3.MoveTowards(enemyTransform.position, playerTransform.position, enemySpeed);
+        // https://unitybeginner.tistory.com/50 ¿¡¼­ °¡Á®¿È
+        Vector2 direction = new(
+            transform.position.x - playerTransform.position.x,
+            transform.position.y - playerTransform.position.y
+        );
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion angleAxis = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+        Quaternion rotation = Quaternion.Slerp(transform.rotation, angleAxis, rotateSpeed * Time.deltaTime);
+        transform.rotation = rotation;
+    }
+
+    private void Move()
+    {
+        transform.Translate(enemySpeed * Time.deltaTime * Vector2.down, Space.Self);
     }
 }
