58256263166e534a7de7f34e8fe1aa6da1f1f384
Vedellan <61517039+Vedellan@users.noreply.github.com>
2023-11-13 23:01:11 +0900

    DOTween 설치
    - 원활한 로비 애니메이팅을 위한 DOTween 설치
    - 이를 이용해 MoveOnFirst.cs를 생성했다.
    - UI는 다른 DOTween을 사용해야 하므로 UIMoveOnFirst.cs를 생성했다. 게임 타이틀은 1초 후에 내려온다.

---
+++ Assets/03. Scripts/Lobby/UIMoveOnFirst.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public class UIMoveOnFirst : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 firstPosition;
+    [SerializeField]
+    private Vector2 targetPosition;
+    [SerializeField]
+    private float duration;
+    [SerializeField]
+    private float startTime;
+
+    private RectTransform rect;
+
+    private void Start()
+    {
+        /*rect = GetComponent<RectTransform>();
+
+        rect.anchoredPosition = firstPosition;
+
+        rect.DOAnchorPos(targetPosition, duration)
+            .SetEase(Ease.OutQuad);*/
+
+        StartCoroutine(MoveWithStartTime(startTime));
+    }
+
+    IEnumerator MoveWithStartTime(float time)
+    {
+        rect = GetComponent<RectTransform>();
+        rect.anchoredPosition = firstPosition;
+
+        yield return new WaitForSeconds(time);
+
+        rect.DOAnchorPos(targetPosition, duration)
+            .SetEase(Ease.OutQuad);
+    }
+}
