b1f1d9d7b54c411644277ee93d2c96a9d4a690dd
Vedellan <61517039+Vedellan@users.noreply.github.com>
2023-11-17 15:52:06 +0900

    파일 계층 구조 해결
    - Item과 KeyItem 등의 폴더 계층 구조가 달랐던 문제 해결

---
+++ Assets/03. Scripts/Item/Item/Items/Item.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+namespace ItemSpacew
+{
+    public class Item : MonoBehaviour
+    {
+        // 변하지 않는 정보들
+        public ItemData itemData;
+        // 변하는 정보들 (각 아이템마다 적용)
+        public int count = 1;
+        public bool isEquiped = false;
+
+        /// <summary>
+        /// 아이템을 사용한다.
+        /// </summary>
+        public virtual void ActivateItem()
+        {
+
+        }
+
+
+        /// <summary>
+        /// 아이템을 해제한다. (Equip, Hand에서 사용)
+        /// </summary>
+        public virtual void DeactivateItem()
+        {
+
+        }
+    }
+}
+
