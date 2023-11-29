1357476ad79f9edbfd0146e44ac22712af145d5d
YIM2UL2ET <132066506+YIM2UL2ET@users.noreply.github.com>
2023-11-29 20:47:30 +0900

    맵 구조 변경 & Stage1BombWall 스크립트 작업 +@

--- Assets/03. Scripts/Puzzle_Logic_Script/Stage1_Script/Stage_Clear_Button.cs
+++ Assets/03. Scripts/Puzzle_Logic_Script/Stage1_Script/Stage_Clear_Button.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class Stage_Clear_Button : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Key를 사용하면 스테이지 클리어 + Roots 클래스에 있는 DisapearRoots() 함수 사용하여 root들 사라지게 함.
 }
