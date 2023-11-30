bad8ad19e354060f3cb778cb19196108805a8dac
Vedellan <61517039+Vedellan@users.noreply.github.com>
2023-12-01 02:46:44 +0900

    튜토리얼 맵 생성
    - 간단하게 흙바닥을 깐 튜토리얼 맵 생성
    - 걸어다닐 때 따라다닐 카메라 움직임 스크립트를 새로 작성

---
+++ Assets/03. Scripts/Camera/LandCamera.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class LandCamera : MonoBehaviour
+{
+    [SerializeField] Transform playerTr;
+    [SerializeField] Transform BG;
+
+    public float followSpeed = 0.5f;
+
+    Vector3 targetPos;
+    public Vector3 offset = new(0f, 0f, 0f);
+    [SerializeField] float minX;
+    [SerializeField] float minY;
+    [SerializeField] float maxX;
+    [SerializeField] float maxY;
+
+    private void Start()
+    {
+        playerTr = GameObject.FindWithTag("Player").transform;
+        BG = GameObject.FindWithTag("BG").transform;
+        targetPos = new(0f, 0f, transform.position.z);
+
+        minX = BG.position.x - (BG.localScale.x / 2);
+        minY = BG.position.y - (BG.localScale.y / 2);
+        maxX = BG.position.x + (BG.localScale.x / 2);
+        maxY = BG.position.y + (BG.localScale.y / 2);
+    }
+
+    private void LateUpdate()
+    {
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        targetPos.x = Mathf.Clamp(playerTr.position.x, minX, maxX);
+        // targetPos.y = Mathf.Clamp(playerTr.position.y, minY, maxY);
+        targetPos.y = 0f;
+        transform.position = targetPos + offset;
+    }
+
+    void SmoothFollowPlayer(float speed)
+    {
+        targetPos.x = Mathf.Clamp(playerTr.position.x, minX, maxX);
+        targetPos.y = Mathf.Clamp(playerTr.position.y, minY, maxY);
+        transform.position = Vector3.Lerp(transform.position, targetPos, speed) + offset;
+    }
+}
